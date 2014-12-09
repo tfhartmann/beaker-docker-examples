eb65195a3442418eb726278852ff93846219a2f9
Tim Hartmann <tfhartmann@gmail.com>
2014-12-09 09:59:35 -0500

    Added package and env var for nokogiri

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,6 +19,17 @@ node default {
     ensure  => installed,
     require => Package['epel-release'],
   }
+  # device mapper needs to be upgraded to support docker 1.3.x in EPEL
+  # This should have been in the depends of the RPM but it's not
+  package { 'device-mapper':
+    ensure => latest,
+    before => Class['docker'],
+  }
+  file { '/etc/profile.d/NOKOGIRI_USE_SYSTEM_LIBRARIES.sh':
+    ensure  => file,
+    content => 'export NOKOGIRI_USE_SYSTEM_LIBRARIES=1',
+    before  => Class['docker'],
+  }
   class { 'docker': }
   class { 'rbenv': }
   rbenv::plugin { 'sstephenson/ruby-build': }
